75cc8fdf6e07e33e4db1a549cea9c3dd20149b18
SergioG19 <gonzalezsimancasseda@uvm.edu.ve>
2023-11-14 00:54:52 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Template from './template/script.js';
-import api from './template/api.js'
-import './Style/style.css'
+import api from './template/api.js';
+import './Style/style.css';
 
 (async function App() {
     const main = null || document.getElementById('main');
